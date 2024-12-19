0d9f29158cf3779c9a03114584ecb30f0696c82a
Rohan Bhaumik <rohanbhaumik@FVFHQ2MKQ05P.local>
2024-12-19 10:44:05 +0530

    intentionally failing a test

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -3,6 +3,7 @@ from .calculator import Calculator
 
 def test_add():
     assert Calculator.add(1, 2) == 3.0
+    assert Calculator.add(1, 1) == 3.0
     assert Calculator.add(1.0, 2.0) == 3.0
     assert Calculator.add(0, 2.0) == 2.0
     assert Calculator.add(2.0, 0) == 2.0
@@ -24,8 +25,9 @@ def test_multiply():
     assert Calculator.multiply(-4, 2.0) == -8.0
 
 def test_divide():
-    # assert Calculator.divide(1, 2) == 0.5
+    assert Calculator.divide(1, 2) == 0.5
     assert Calculator.divide(1.0, 2.0) == 0.5
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
-    # assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
+    assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
+
